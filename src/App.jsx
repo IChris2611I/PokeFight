4047bc76721c0929f87c745a0897ccc0cdb0fc12
IChris2611I <pachris2611@gmail.com>
2024-04-15 11:35:58 +0200

    MAJ Archy project

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,44 @@ function App() {
 
   return (
     <>
-      <h1>PokeFight</h1>
+      <main>        
+        <div className="fight__scene">
+          <div className="fight__scene__top">
+            <div className="status__Bar">
+              <div className="pokemon__name">
+                <h1>Pikachu</h1>
+              </div>
+            </div>
+            <div className="pokemon__image">
+              <img src="#" alt="Pokemon"/>
+            </div>
+          </div>
+
+          <div className="fight__scene__bot">
+          <div className="status__Bar">
+              <div className="pokemon__name">
+                <h1>Pikachu</h1>
+              </div>
+            </div>
+            <div className="pokemon__image">
+              <img src="#" alt="Pokemon"/>
+            </div>
+          </div>
+        </div>
+
+        <div className="controls">
+          <div className="show__text">
+              <p>Show Text Combat</p>
+          </div>
+          <div className="button__block">
+            <button><p>Attack 1</p></button>
+            <button><p>Attack 2</p></button>
+            <button><p>Attack 3</p></button>
+            <button><p>Pokemon</p></button>
+          </div>
+        </div>
+
+      </main>
     </>
   )
 }
